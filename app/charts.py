e61708b86d6e18f9d0481e6e6caac76d50d84d74
root <root@LAPTOP-FJF29GNV>
2024-02-18 19:24:17 -0500

    Add docs

---
+++ app/charts.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(name, labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig(f'./imgs/{name}.png')
+  plt.close()
+
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.savefig('pie.png')
+  plt.close()
+
+if __name__ == '__main__':
+  labels = ['a', 'b', 'c']
+  values = [10, 40, 800]
+  # generate_bar_chart(labels, values)
+  generate_pie_chart(labels, values)
+'''import matplotlib.pyplot as plt
+import numpy as np
+
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()  # Corregido para desempaquetar correctamente fig y ax
+    ax.bar(labels, values)
+    plt.show()
+    
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    plt.show()
+# Definiendo los labels y valores fuera de la función
+#labels = ['a', 'b', 'c']
+#values = [100, 40, 800]
+
+# Llamando a la función directamente sin el bloque if __name__ == '__main__':
+generate_bar_chart(labels, values)
+#generate_pie_chart(labels, values)
+
+# %%'''
---
+++ app/example.py
@@ -0,0 +1,4 @@
+import app.main as main
+
+print(main.data)
+main.run()
